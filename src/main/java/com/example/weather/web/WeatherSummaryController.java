19f4631f4bc2960a122027b065b81d4b01397d54
Stephane Nicoll <snicoll@pivotal.io>
2016-05-09 12:24:20 +0200

    Core container improvements demonstration

--- src/main/java/com/example/weather/web/WeatherSummaryController.java
+++ src/main/java/com/example/weather/web/WeatherSummaryController.java
@@ -5,15 +5,14 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.weather.WeatherAppProperties;
+import com.example.weather.integration.ows.Weather;
+import com.example.weather.integration.ows.WeatherService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.example.weather.WeatherAppProperties;
-import com.example.weather.integration.ows.Weather;
-import com.example.weather.integration.ows.WeatherService;
 
 @Controller
 @RequestMapping("/")
@@ -23,7 +22,6 @@ public class WeatherSummaryController {
 
 	private final WeatherAppProperties properties;
 
-	@Autowired
 	public WeatherSummaryController(WeatherService weatherService, WeatherAppProperties properties) {
 		this.weatherService = weatherService;
 		this.properties = properties;
